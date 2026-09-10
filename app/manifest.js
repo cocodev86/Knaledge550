77aadd1b86f3f673c8aa8b055d0eaeb03209af5b
Demetrius Moreland <coco@growthwithcoco.com>
2026-09-10 16:14:10 -0400

    Add KNALEDGE550 web app manifest

---
+++ app/manifest.js
@@ -0,0 +1,11 @@
+export default function manifest() {
+  return {
+    name: "KNALEDGE550",
+    short_name: "550",
+    description: "Official KNALEDGE550 artist site.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#040404",
+    theme_color: "#040404",
+  };
+}
